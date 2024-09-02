899337d6d96c11bf17a157d63dc436f1c14b4e89
宇 <45961471+paokuwansui@users.noreply.github.com>
2024-09-02 15:58:44 +0800

    Update 加密器.c

--- 加密器.c
+++ 加密器.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 static char base64_table[] = "Ag9jCXabcJKLV2345WmnopuvwxYZklMhi78NOPrstTUByz0defDEFGHI16+/QRSq";
-static char *key = "AeB&79!ra0(3*)";
+static char key[] = "AeB&79!ra0(3*)";
 
-void base64_encode(unsigned char *input, int length, char *output) {
+void base64_encode(unsigned char *input, int length, unsigned char *output) {
     int i, j;
     for (i = 0, j = 0; i < length;) {
         unsigned int octet_a = i < length ? input[i++] : 0;
@@ -22,19 +22,18 @@ void base64_encode(unsigned char *input, int length, char *output) {
     output[j] = '\0';
 }
 
-void xor_encrypt_decrypt(char *input, char *output, char *key, int len) {
+void xor_encrypt_decrypt(unsigned char *input, unsigned char *output, char *key, int len) {
     int key_length = strlen(key);
     for (int i = 0; i < len; i++) {
         output[i] = input[i] ^ key[i % key_length];
     }
     output[len] = '\0';
 }
-
 int main() {
     unsigned char buf[] = "your shellcode";
 
-    char encoded[2048];
-    char encrypted[2048];
+    unsigned char encoded[2048];
+    unsigned char encrypted[2048];
     xor_encrypt_decrypt(buf, encrypted, key, sizeof(buf) / sizeof(buf[0]) - 1);
     base64_encode(encrypted, sizeof(buf) / sizeof(buf[0]), encoded);
     printf("shllcode密文: %s\n", encoded);
